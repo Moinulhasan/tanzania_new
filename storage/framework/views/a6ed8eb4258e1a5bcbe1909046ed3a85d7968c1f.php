add38ac52dc33b8c189ba36f1420ae556ce2999a
Moinul Islam <moinul20@iotait.tech>
2023-01-23 00:40:06 +0600

    update section

---
+++ storage/framework/views/a6ed8eb4258e1a5bcbe1909046ed3a85d7968c1f.php
@@ -0,0 +1,26 @@
+<div class="hotel-star">
+    <?php
+        echo hotel_star($post['hotel_star'])
+    ?>
+</div>
+<h1 class="post-title">
+    <?php echo add_wishlist_box($post['id'], GMZ_SERVICE_HOTEL); ?>
+    <?php echo e(get_translate($post['post_title'])); ?>
+
+    <?php if($post['is_featured'] == 'on'): ?>
+        <span class="is-featured"><?php echo e(__('Featured')); ?></span>
+    <?php endif; ?>
+</h1>
+<p class="location">
+    <i class="fal fa-map-marker-alt"></i> <?php echo e(get_translate($post['location_address'])); ?>
+
+</p>
+<?php if(!empty($post['rating'])): ?>
+    <?php
+        $review_number = get_comment_number($post['id'], $post['post_type']);
+    ?>
+    <div class="count-reviews">
+        <span><?php echo e($post['rating']); ?><small>/5</small><i class="fa fa-star"></i></span> <?php echo e(sprintf(_n(__('from %s review'), __('from %s reviews'), $review_number), $review_number)); ?>
+
+    </div>
+<?php endif; ?><?php /**PATH C:\xampp\htdocs\nfz\tanzania\app\Modules/Frontend/Views/services/hotel/single/header.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/e5950c40ddedfa41462ca3066ef1aecdc9be681e.php
@@ -0,0 +1,27 @@
+<?php
+    $enable = get_option($post['post_type'] . '_show_partner_info', 'on');
+?>
+
+<?php if($enable == 'on'): ?>
+    <?php
+    $author = $post['author'];
+    $avatar = get_user_avatar($author, [100, 100]);
+    $userName = get_user_name($author);
+    $userData = get_user_data($author);
+    ?>
+    <div class="partner-info">
+        <div class="info-head">
+            <a href="<?php echo e(url('author/' . $author)); ?>">
+                <img src="<?php echo e($avatar); ?>" alt="avatar" />
+                <p>
+                    <span class="username"><?php echo e(sprintf(__('Posted by %s'), $userName)); ?></span>
+                    <span class="address"><?php echo e($userData['address']); ?></span>
+                </p>
+            </a>
+        </div>
+        <div class="info-body">
+            <?php echo e(nl2br($userData['description'])); ?>
+
+        </div>
+    </div>
+<?php endif; ?><?php /**PATH C:\xampp\htdocs\nfz\tanzania\app\Modules/Frontend/Views/components/sections/partner-info.blade.php ENDPATH**/ ?>
